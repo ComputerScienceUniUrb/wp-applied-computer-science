567719ec2bb2da4854bd685ea0ca148473c0cbd4
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-02-11 15:50:35 +0100

    Switch contact with webmail access in social bar

--- src/template-parts/social-bar.php
+++ src/template-parts/social-bar.php
@@ -4,7 +4,7 @@
         <a href="https://www.facebook.com/InfoAppl/"><i class="fab fa-facebook-f"></i></a>
         <a href="https://twitter.com/InfoAppl"><i class="fab fa-twitter"></i></a>
         <a href="skype:$cdl.informatica?call"><i class="fab fa-skype"></i></a>
-        <a href="http://mail.google.com/a/campus.uniurb.it"><i class="fas fa-envelope"></i></a>
+        <a href="mailto:cdl.informatica@uniurb.it"><i class="fas fa-envelope"></i></a>
         <a href="<?php bloginfo('rss2_url'); ?>"><i class="fas fa-rss"></i></a>
     </div>
 </div>

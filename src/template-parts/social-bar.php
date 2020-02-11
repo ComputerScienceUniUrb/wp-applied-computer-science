bc347b5b23612ef79a9364695339569dedf0c054
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-02-11 11:29:57 +0100

    Fix top bar

--- src/template-parts/social-bar.php
+++ src/template-parts/social-bar.php
@@ -1,12 +1,11 @@
-<!-- Social bar -->
+<!-- .topbar -->
 <div class="topbar">
-	<ul class="socials">
-        <li><a target="_blank" href="https://www.facebook.com/InfoAppl/"><i class="fab fa-facebook-f"></i></a></li>
-        <li><a target="_blank" href="https://twitter.com/InfoAppl"><i class="fab fa-twitter"></i></a></li>
-        <li><a href="skype:$cdl.informatica?call"><i class="fab fa-skype"></i></a></li>
-        <!-- <li><a href="#"><i class="fab fa-foursquare"></i></a></li> -->
-        <li><a target="_blank" href="http://mail.google.com/a/campus.uniurb.it"><i class="fas fa-envelope"></i></a></li>
-        <li><a target="_blank" href="<?php bloginfo('rss2_url'); ?>"><i class="fas fa-rss"></i></a></li>
-    </ul>
+	<div class="socials">
+        <a href="https://www.facebook.com/InfoAppl/"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com/InfoAppl"><i class="fab fa-twitter"></i></a>
+        <a href="skype:$cdl.informatica?call"><i class="fab fa-skype"></i></a>
+        <a href="http://mail.google.com/a/campus.uniurb.it"><i class="fas fa-envelope"></i></a>
+        <a href="<?php bloginfo('rss2_url'); ?>"><i class="fas fa-rss"></i></a>
+    </div>
 </div>
-<!-- Social bar -->
+<!-- /.topbar -->

7d1617890499e26e8999568c986bc13ccab9a062
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-02-11 13:57:11 +0100

    Update footer style and copy

--- src/footer.php
+++ src/footer.php
@@ -1,28 +1,15 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package applied-computer-science
- */
-
-?>
-
 	</div><!-- #content -->
 
 	<footer id="footer" class="site-footer">
 		<div class="prefooter">
 			<div class="container">
 				<div class="row">
-					<?php dynamic_sidebar( 'footer' ); ?>
+					<?php dynamic_sidebar('footer'); ?>
 				</div>
 			</div>
 		</div>
 		<div class="copyright">
-			<p>&copy; <?php echo date('Y'); ?> | Developed with <i class="fas fa-heart"></i> by <a class="grey" target="_blank" href="https://github.com/damiandominella">Damian Dominella</a></p>
+			<p>Informatica Applicata, Università di Urbino &copy; <?php echo date('Y'); ?> | <a class="grey" target="_blank" href="https://github.com/ComputerScienceUniUrb/wp-applied-computer-science">Fork me on GitHub!</a></p>
 		</div>
 	</footer><!-- #footer -->
 </div><!-- #page -->

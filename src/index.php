5e3b7d9627043846081c35b7b5675cf0885b3b42
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-03-15 12:36:50 +0100

    Clean up single page

--- src/index.php
+++ src/index.php
@@ -16,6 +16,23 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
+
+	<?php if((is_front_page() || is_singular()) && have_posts()) { ?>
+	<?php the_post(); ?>
+
+	<?php if(has_post_thumbnail()) { ?>
+		<section class="hero">
+			<div class="container">
+				<?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large'); ?>
+				<img src="<?php echo $thumb[0]; ?>" width="<?php echo $thumb[1]; ?>" height="<?php echo $thumb[2]; ?>" srcset="<?php echo wp_get_attachment_image_srcset(get_post_thumbnail_id()); ?>" alt="" />
+			</div>
+		</section>
+
+	<?php } ?>
+	
+	<?php rewind_posts(); ?>
+<?php } ?>
+
 		<main id="main" class="site-main">
 
 		<?php
@@ -29,15 +46,9 @@ get_header();
 				<?php
 			endif;
 
-			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
--- src/single.php
+++ src/single.php
@@ -6,19 +6,34 @@
  *
  * @package applied-computer-science
  */
-
 get_header();
 ?>
 
 	<div id="primary" class="content-area">
+
+<?php if((is_front_page() || is_singular()) && have_posts()) { ?>
+	<?php the_post(); ?>
+
+	<?php if(has_post_thumbnail()) { ?>
+		<section class="hero">
+			<div class="container">
+				<img src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id(), 'large'); ?>" srcset="<?php echo wp_get_attachment_image_srcset(get_post_thumbnail_id()); ?>" alt="" />
+			</div>
+		</section>
+
+	<?php } ?>
+	
+	<?php rewind_posts(); ?>
+<?php } ?>
+
 		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<?php while(have_posts()) { ?>
+			<?php the_post(); ?>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			<?php get_template_part('template-parts/content', get_post_type()); ?>
 
+			<?php
 			// if (get_post_type() === 'bulletin_board' || get_post_type() === 'seminars') {
 
 			// } else {
@@ -29,9 +44,9 @@ get_header();
 			// 		comments_template();
 			// 	endif;
 			// }
+			?>
 
-		endwhile; // End of the loop.
-		?>
+		<?php } ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
